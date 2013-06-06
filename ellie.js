60398d2463d2b16e3e2640ab3fd3b0279bb6c9ca
Philipp Waldhauer <ich@philippwaldhauer.de>
2013-06-06 03:09:35 +0200

    Initial import

---
+++ ellie.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+var ellie = new (require('./lib/Elizabeth').Elizabeth);
+
+var command = null;
+if(process.argv.length > 2) {
+	command = process.argv[2];
+}
+
+if(command == 'init') {
+	ellie.startInit();
+} else if(command == 'show') {
+	ellie.showProfile();
+} else if(command == 'export') {
+	ellie.startExport();
+} else {
+	ellie.printHelp();
+}
